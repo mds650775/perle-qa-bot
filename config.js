ee88dbade4c6d740971019c941a31cca2b8f61b7
mds650775 <78658162+mds650775@users.noreply.github.com>
2026-02-03 12:37:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ require("dotenv").config();
 module.exports = {
   DISCORD_TOKEN: process.env.DISCORD_TOKEN || "",
   TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN || "",
-  OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY || ""
+  OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY || "",
+  OPENROUTER_API_URL: process.env.OPENROUTER_API_URL || ""
 };
